a4a71c6ebbc2714c93815c790a0faf8aa3a5c9c9
Pedro Mendonça <althander@gmail.com>
2024-01-17 15:56:44 -0300

    Atividade Barbearia
    Primeira versão

--- script.js
+++ script.js
@@ -19,27 +19,81 @@ const barbearia = {
   
   
 function buscaCortePorId(id) {
+    
+    for (let i = 0; i < barbearia.cortes.length; i++){
+        if (id === barbearia.cortes[i].id) {
+            return barbearia.cortes[i]
+        }       
+    }
+
+    return 'Corte não encontrado'
 }
 
 function buscaBarbaPorId(id) {
+
+    for (let i = 0; i < barbearia.barbas.length; i++){
+        if (id === barbearia.barbas[i].id) {
+            return barbearia.barbas[i]
+        }       
+    }
+
+    return 'Barba não encontrada'
 }
 
 function verificaStatusBarbearia() {
+    if (barbearia.estaAberto){
+        return 'Estamos abertos'
+    }
+
+    return 'Estamos fechados.'
 }
 
 function retornaTodosCortes() {
+    let todosCortes = barbearia.cortes
+
+    return todosCortes
 }
 
 function retornaTodasBarbas() {
+    let todasBarbas = barbearia.barbas
+
+    return todasBarbas
 }
 
+
 function criaPedido(nomeCliente, corteId, barbaId) {
+    let corteEncontrado  = buscaCortePorId(corteId)
+    let barbaEncontrada  = buscaBarbaPorId(barbaId)
+
+    let pedido = {
+        nome: nomeCliente,
+        pedidoCorte: corteEncontrado.tipo,
+        pedidoCortePreco: corteEncontrado.valor,
+        pedidoBarba: barbaEncontrada.tipo,
+        pedidoBarbaPreco: barbaEncontrada.valor,
+    }
+
+    return pedido
 }
 
 function atualizarServico(lista, id, valor, tipo) {
+    
+    for (let i = 0; i < lista.length; i++) {
+        if (id === lista[i].id) {
+            lista[i].tipo = tipo
+            lista[i].valor = valor
+        }
+    }
+
+    
+    return lista
 }
 
 function calculaTotal(pedido) {
+    const cortePreco = pedido.pedidoCortePreco
+    const barbaPreco = pedido.pedidoBarbaPreco
+
+    return cortePreco + barbaPreco
 }
   
   
